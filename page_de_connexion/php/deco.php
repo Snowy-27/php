0468a901bfecddfba3f34c777b4273ca682930e5
Dan habib <57772926+Snowy-27@users.noreply.github.com>
2020-04-26 09:48:56 +0200

    Add files via upload

---
+++ page_de_connexion/php/deco.php
@@ -0,0 +1,16 @@
+<?php  session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+     <head>
+          <meta charset="utf-8">
+          <title></title>
+     </head>
+     <body>
+          <?php
+          session_destroy();
+          header('Location: ../index.php');
+          ?>
+
+     </body>
+</html>
